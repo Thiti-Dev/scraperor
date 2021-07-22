756c12bb226bf2e10beafc099b6c2c551771fb94
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-23 04:23:52 +0700

    schema.resolvers: implemented (Mutation)ScrapeNow

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,9 +6,22 @@ package graph
 import (
 	"context"
 
+	"github.com/Thiti-Dev/scraperor/core"
 	"github.com/Thiti-Dev/scraperor/graph/generated"
+	"github.com/Thiti-Dev/scraperor/graph/model"
 	"github.com/Thiti-Dev/scraperor/models"
 )
+
+func (r *mutationResolver) ScrapeNow(ctx context.Context, input *model.ScrapeInput) (*model.ScrapeOutput, error) {
+	
+	engineEntity := core.CreateEngineEntity()
+	engineEntity.AddTask(input.TargetURL,input.Pointer)
+	
+	elements := engineEntity.Scrape()
+	return &model.ScrapeOutput{
+		Elements: elements,
+	},nil
+}
 
 func (r *queryResolver) Users(ctx context.Context) ([]*models.User, error) {
 	/*user := new(model.User)
@@ -25,15 +38,11 @@ func (r *queryResolver) Users(ctx context.Context) ([]*models.User, error) {
 	return users, nil
 }
 
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
-type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
 type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
