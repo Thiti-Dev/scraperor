45bf4440b38fe9425af90d6f9d2499b5f9d5c996
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-22 18:55:14 +0700

    implemented env extraction phase of the batch script with err-handler

--- run_create-seed.cmd
+++ run_create-seed.cmd
@@ -1,3 +1,14 @@
 @echo off
-for /f "delims== tokens=1*" %%A in (.env) do set POSTGRESQL_URL=%%B
+for /f "delims== tokens=1*" %%A in (.env) do (
+    if %%A equ POSTGRESQL_URL (
+        set POSTGRESQL_URL=%%B
+        goto process
+    )
+)
+:process
+if [%POSTGRESQL_URL%] == [] (
+    echo No .env file found with the POSTGRESQL_URL variable
+    goto:eof
+)
+
 psql -d %POSTGRESQL_URL% -f .\postgres\seed.sql
--- run_migration.cmd
+++ run_migration.cmd
@@ -1,3 +1,15 @@
-for /f "delims== tokens=1*" %%A in (.env) do set POSTGRESQL_URL=%%B
+@echo off
+for /f "delims== tokens=1*" %%A in (.env) do (
+    if %%A equ POSTGRESQL_URL (
+        set POSTGRESQL_URL=%%B
+        goto process
+    )
+)
+:process
+if [%POSTGRESQL_URL%] == [] (
+    echo No .env file found with the POSTGRESQL_URL variable
+    goto:eof
+)
+
 
 migrate -path "postgres/migrations" -database %POSTGRESQL_URL% %*
