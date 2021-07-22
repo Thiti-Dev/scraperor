4d7814eb6fdae047b6cc58790272e54eb8f695a3
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-22 19:45:33 +0700

    Resolver -> added intregity for the UsersRepository for injection

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/Thiti-Dev/scraperor/postgres"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	UsersRepository postgres.UsersRepository
+}
