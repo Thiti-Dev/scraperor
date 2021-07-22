1485abf47695e12aa929fdeae0360c88132e9546
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-22 19:46:04 +0700

    gql: implemented (Query)Users

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -7,17 +7,22 @@ import (
 	"context"
 
 	"github.com/Thiti-Dev/scraperor/graph/generated"
-	"github.com/Thiti-Dev/scraperor/graph/model"
+	"github.com/Thiti-Dev/scraperor/models"
 )
 
-func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
-	user := new(model.User)
+func (r *queryResolver) Users(ctx context.Context) ([]*models.User, error) {
+	/*user := new(model.User)
 	user.Email = "Thiti Mahawannakit"
 	user.ID = "xxx64"
 
 	var users []*model.User
 	users = append(users, user)
-	return users,nil
+	return users,nil*/
+	users, err := r.UsersRepository.GetUsers()
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 
 // Query returns generated.QueryResolver implementation.
@@ -25,4 +30,10 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type queryResolver struct{ *Resolver }
 
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
 type mutationResolver struct{ *Resolver }
