e22ce8a464d307c12e08b7ab0b178f0c5d40e74f
Thiti Dev <thiti.mwk.main@gmail.com>
2021-07-22 19:46:47 +0700

    created db_instance on entry and injects in the GQLResolver

--- server.go
+++ server.go
@@ -9,17 +9,33 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Thiti-Dev/scraperor/graph"
 	"github.com/Thiti-Dev/scraperor/graph/generated"
+	"github.com/Thiti-Dev/scraperor/postgres"
+	"github.com/go-pg/pg/v10"
 )
 
 const defaultPort = "8080"
 
 func main() {
+
+	DB := postgres.New(&pg.Options{
+		Addr:     ":5432",
+		User:     "postgres",
+		Password: "root",
+		Database: "scraperor",
+	})
+
+	defer DB.Close()
+
+	DB.AddQueryHook(postgres.DBLogger{})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
+		UsersRepository: postgres.UsersRepository{DB:DB},
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
